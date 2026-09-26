76e92eab8a6d51589229c238fa4b76ac0f12bee3
Thulana <imakula.21@cse.mrt.ac.lk>
2026-09-26 16:10:00 +0530

    fix

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -50,6 +50,5 @@ module "ecs" {
   task_cpu                 = var.task_cpu
   task_memory              = var.task_memory
   image_tag                = var.image_tag
-  db_address  = module.rds.db_address
-  db_password = module.rds.db_password
+  db_password              = module.rds.db_password
 }
