29735c01aaea072f99d5e690e14cfabe6a1e1be3
Thulana <imakula.21@cse.mrt.ac.lk>
2026-09-27 01:13:59 +0530

    fix syntax error

--- terraform/modules/scaling/variables.tf
+++ terraform/modules/scaling/variables.tf
@@ -5,9 +5,32 @@ variable "task_family"        { type = string }
 variable "execution_role_arn" { type = string }
 variable "task_role_arn"      { type = string }
 
-variable "cpu_high_threshold"    { type = number, default = 75 }
-variable "cpu_low_threshold"     { type = number, default = 20 }
-variable "memory_high_threshold" { type = number, default = 75 }
-variable "memory_low_threshold"  { type = number, default = 20 }
-variable "evaluation_periods"    { type = number, default = 3 }
-variable "period"                { type = number, default = 60 }
+variable "cpu_high_threshold" {
+  type    = number
+  default = 75
+}
+
+variable "cpu_low_threshold" {
+  type    = number
+  default = 20
+}
+
+variable "memory_high_threshold" {
+  type    = number
+  default = 75
+}
+
+variable "memory_low_threshold" {
+  type    = number
+  default = 20
+}
+
+variable "evaluation_periods" {
+  type    = number
+  default = 3
+}
+
+variable "period" {
+  type    = number
+  default = 60
+}
