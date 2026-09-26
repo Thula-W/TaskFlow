08b9bc6cef365de912c4cede9b548cd190a6b723
Thulana <imakula.21@cse.mrt.ac.lk>
2026-09-26 12:24:28 +0530

    lint issues fix 2

--- terraform/envs/prod/backend.tf
+++ terraform/envs/prod/backend.tf
@@ -13,9 +13,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "taskflow-tfstate-935710521571"  
+    bucket         = "taskflow-tfstate-935710521571"
     key            = "prod/terraform.tfstate"
-    region         = "ap-south-1"                 
+    region         = "ap-south-1"
     dynamodb_table = "taskflow-tf-locks"
     encrypt        = true
   }
